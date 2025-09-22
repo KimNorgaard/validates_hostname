e06f0e72ad0d2a369eadfa96e72576e27e42b7bd
Kim Nørgaard <jasen@jasen.dk>
2025-09-22 10:44:21 +0200

    fix: added back error messages and enabled monkey patching

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,15 @@
-# frozen_string_literal: true
+require 'simplecov'
+SimpleCov.start
 
+require 'rspec/core'
 require 'bundler/setup'
-require 'logger'
 require 'active_model'
 require 'rspec/collection_matchers'
-
 require 'validates_hostname'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
